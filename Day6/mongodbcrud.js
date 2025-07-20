8658d8b398f8a7e9b12b87b6db87bb7b4645b5a7
Komalanap07@gmail.com <komalanap07@gmail.com>
2025-07-20 11:14:32 -1200

    Day6-MongoDB CRUD

---
+++ Day6/mongodbcrud.js
@@ -0,0 +1,36 @@
+// use myDB;
+//create opration
+db.createCollection("users") 
+
+//insertion
+db.users.insertOne({
+  name: "Komal",
+  email: "komal@example.com",
+  age: 22
+})
+
+db.users.insertMany([
+  { name: "Rahul", email: "rahul@mail.com", age: 25 },
+  { name: "Anita", email: "anita@mail.com", age: 23 }
+])
+db.users.find()                       // all documents
+db.users.find({ name: "Komal" })     // specific match
+db.users.findOne({ age: 22 })        // single result
+
+//updation
+// Update one field
+db.users.updateOne(
+  { name: "Komal" },
+  { $set: { age: 23 } }
+)
+
+// Update multiple documents
+db.users.updateMany(
+  { age: { $gt: 22 } },
+  { $set: { status: "verified" } }
+)
+// Delete one
+db.users.deleteOne({ name: "Komal" })
+
+// Delete many
+db.users.deleteMany({ age: { $lt: 24 } })
